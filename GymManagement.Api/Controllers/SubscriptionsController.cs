9eea67ab4c313e670bc8c5b885ab5ad41631c0b9
Saheed Adeniji <adeniji4success@gmail.com>
2024-01-01 20:44:54 +0100

    Add project files.

---
+++ GymManagement.Api/Controllers/SubscriptionsController.cs
@@ -0,0 +1,26 @@
+using GymManagement.Application.Services;
+using GymManagement.Contracts.Subscriptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagement.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SubscriptionsController : ControllerBase
+{
+    private readonly ISubscriptionsService _subscriptionsService;
+
+    public SubscriptionsController(ISubscriptionsService subscriptionsService)
+    {
+        _subscriptionsService = subscriptionsService;
+    }
+
+    [HttpPost]
+    public IActionResult CreateSubscription(CreateSubscriptionRequest request)
+    {
+        var subscriptionId = _subscriptionsService.CreateSubscription(request.SubscriptionType.ToString(), request.AdminId);
+        
+        var response = new SubscriptionResponse(subscriptionId, request.SubscriptionType);
+        return Ok(response);
+    }
+}
---
+++ GymManagement.Application/Services/ISubscriptionsService.cs
@@ -0,0 +1,7 @@
+namespace GymManagement.Application.Services
+{
+    public interface ISubscriptionsService
+    {
+        Guid CreateSubscription(string subscriptionType, Guid adminId);
+    }
+}
